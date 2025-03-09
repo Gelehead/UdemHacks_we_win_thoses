b50862fb84d82c37fb44d06e62baa7ff33a0332e
KarimOuatt <abdoul.karim.ouattara.1@umontreal.ca>
2025-03-09 01:28:14 -0500

    gd gkmdg
    ggdt gb

--- src/website/BackEnd/script.js
+++ src/website/BackEnd/script.js
@@ -1,27 +1,37 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const videoInput = document.getElementById("videoInput");
-    const videoPlayer = document.getElementById("videoPlayer");
-    const processButton = document.getElementById("processButton");
+  const videoInput = document.getElementById("videoInput");
+  const videoPlayer = document.getElementById("videoPlayer");
+  const processButton = document.getElementById("processButton");
 
-    videoInput.addEventListener("change", function (event) {
-        const file = event.target.files[0];
-        if (file) {
-            const url = URL.createObjectURL(file);
-            videoPlayer.src = url;
-            processButton.disabled = false;
-        
-        }
-        a = fetch("http://127.0.0.1:5000",{method: "POST", body: JSON.stringify({file: file}), headers: {"Content-Type": "application/json"}})
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .catch(error => console.log(error));
-    });
+  videoInput.addEventListener("change", function (event) {
+    const file = event.target.files[0];
+    let base64String = "";
+    if (file) {
+      const url = URL.createObjectURL(file);
+      videoPlayer.src = url;
+      processButton.disabled = false;
+      const reader = new FileReader();
+      reader.onloadend = function () {
+        // Le résultat est une URL de données, on prend la partie après la virgule
+        base64String = reader.result.split(",")[1];
+        console.log(base64String); // Affiche la chaîne Base64
+      };
+    }
+    a = fetch("http://127.0.0.1:5000", {
+      method: "POST",
+      body: JSON.stringify({ file: file }),
+      headers: { "Content-Type": "multipart/form-data" },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.log(error));
+  });
 
-    processButton.addEventListener("click", function () {
-        const file = videoInput.files[0];
-        if (file) {
-            alert("Lancement du script pour traiter la vidéo : " + file.name);
-            // Ici, tu pourras appeler ton vrai script plus tard
-        }
-    });
+  processButton.addEventListener("click", function () {
+    const file = videoInput.files[0];
+    if (file) {
+      alert("Lancement du script pour traiter la vidéo : " + file.name);
+      // Ici, tu pourras appeler ton vrai script plus tard
+    }
+  });
 });
--- src/website/app.py
+++ src/website/app.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template, request
 import os
-
+from flask_cors import CORS
+import json
 app = Flask(__name__)
-
+CORS(app)
 @app.route('/')
 def home():
     return render_template('index.html')
-
+@app.route('/', methods=['POST'])
+def maison():
+    print(request.get_data())
+    return json.dumps(request.get_data())
 if __name__ == '__main__':
     app.run(debug=True)
